e8e0cc2f28eb537e1914775de6caddb5c0f73bb4
Azizi1213 <125625536+Azizi1213@users.noreply.github.com>
2023-02-27 12:13:24 -0500

    Update unittesttwo.java
    This is for pull practice

--- src/TestGit/unittesttwo.java
+++ src/TestGit/unittesttwo.java
@@ -3,6 +3,7 @@ package TestGit;
 public class unittesttwo {
 	public static void main(String[] args) {
 		System.out.println("This is for push practice");
+		System.out.println("This is for pull practice");
 	}
 
 }
