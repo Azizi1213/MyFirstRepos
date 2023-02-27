47f5a1df57453e32c6a75c007444d588264dbb7a
Azizi1213 <abdulsamad.azizi07@gmail.com>
2023-02-27 11:57:31 -0500

    new method added

--- src/TestGit/unittestgit.java
+++ src/TestGit/unittestgit.java
@@ -4,12 +4,19 @@ public class unittestgit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 10;
-		int b = 15;
-		int c = a + b;
+//		int a = 10;
+//		int b = 15;
+//		int c = a + b;
+//		
+//		System.out.println(c);
+//		
 		
-		System.out.println(c);
+		System.out.println(testone(15,5));
 
+	}
+	
+	public static int testone(int a, int b) {
+		return a-b;
 	}
 
 }
