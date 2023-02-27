81d2394b5f5c1ac9b1fb5b4e5cc0b2cbc639948a
Azizi1213 <abdulsamad.azizi07@gmail.com>
2023-02-27 13:41:47 -0500

    this is for revert

--- src/TestGit/unittesttwo.java
+++ src/TestGit/unittesttwo.java
@@ -4,6 +4,7 @@ public class unittesttwo {
 	public static void main(String[] args) {
 		System.out.println("This is for push practice");
 		System.out.println("This is for pull practice");
+		System.out.println("This is for revert practice");
 	}
 
 }
